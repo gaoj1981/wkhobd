efd650cc65e9d948c66380247c2beb19e671457a
gaoj1981 <www.woniu8.com@163.com>
2018-10-24 00:07:55 +0800

    设置临时权限

--- obd-core/src/main/java/com/wkhmedical/security/DefaultAuthenticationSuccessHandler.java
+++ obd-core/src/main/java/com/wkhmedical/security/DefaultAuthenticationSuccessHandler.java
@@ -19,23 +19,32 @@ import lombok.Setter;
 public class DefaultAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
 	@Override
-	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-			Authentication authentication) throws ServletException, IOException {
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+			throws ServletException, IOException {
 		if (WebUtil.isAjaxRequest(request)) {
 			WebUtil.setJsonResponseConfig(response);
 			response.setStatus(HttpServletResponse.SC_OK);
 			JSONObject jso = new JSONObject();
 			jso.put("status", "ok");
 			jso.put("type", "account");
-			jso.put("currentAuthority", "admin");
+			// 临时设置权限
+			TUserDetails userDetail = (TUserDetails) authentication.getPrincipal();
+			String uname = userDetail.getUsername();
+			if ("123111111".equals(uname)) {
+				jso.put("currentAuthority", "admin");
+			}
+			else {
+				jso.put("currentAuthority", "user");
+			}
 			PrintWriter out = response.getWriter();
 			out.append(jso.toJSONString());
 			out.flush();
 			out.close();
 			clearAuthenticationAttributes(request);
-		} else {
+		}
+		else {
 			super.onAuthenticationSuccess(request, response, authentication);
 		}
 	}
-	
+
 }
