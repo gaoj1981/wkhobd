641d9a36be8e1920a8792087023f6608c2c8fab5
gaoj1981 <www.woniu8.com@163.com>
2018-10-29 21:22:55 +0800

    添加poi

---
+++ obd-core/src/main/java/com/wkhmedical/constant/EquipConstant.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.wkhmedical.constant;
+
+/**
+ * @author Administrator
+ */
+public class EquipConstant {
+
+	public static String[] equipMainArr = new String[16];
+	static {
+		equipMainArr[0] = "车辆";
+		equipMainArr[1] = "计算机";
+		equipMainArr[2] = "显示器";
+		equipMainArr[3] = "身高体重";
+		equipMainArr[4] = "身高体重-多端口无线同步器";
+		equipMainArr[5] = "血压";
+		equipMainArr[6] = "血压-多端口无线同步器";
+		equipMainArr[7] = "尿液分析";
+		equipMainArr[8] = "尿液分析-多端口无线同步器";
+		equipMainArr[9] = "心电图机";
+		equipMainArr[10] = "B超";
+		equipMainArr[11] = "血球";
+		equipMainArr[12] = "离心机";
+		equipMainArr[13] = "生化";
+		equipMainArr[14] = "车载HIS系统";
+		equipMainArr[15] = "加密狗";
+	}
+
+	public static String[] equipAssistArr = new String[18];
+	static {
+		equipAssistArr[0] = "急救包";
+		equipAssistArr[1] = "不锈钢担架";
+		equipAssistArr[2] = "身份证读卡器";
+		equipAssistArr[3] = "条码打印机";
+		equipAssistArr[4] = "打印机";
+		equipAssistArr[5] = "USB分线器7口";
+		equipAssistArr[6] = "B超-USB分线器3口";
+		equipAssistArr[7] = "串口扫描枪";
+		equipAssistArr[8] = "USB扫描枪";
+		equipAssistArr[9] = "心电图-USB扫描枪";
+		equipAssistArr[10] = "无线路由器";
+		equipAssistArr[11] = "B超-外置无线网卡";
+		equipAssistArr[12] = "血球-串口转USB线";
+		equipAssistArr[13] = "网线";
+		equipAssistArr[14] = "血压-串口线";
+		equipAssistArr[15] = "尿液分析-串口线";
+		equipAssistArr[16] = "插线板";
+		equipAssistArr[17] = "B超-鼠标";
+	}
+}
