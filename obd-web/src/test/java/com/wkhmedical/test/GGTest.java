41c6f071292894d5221cf9c4fc359fb1897e8ca6
gaoj1981 <www.woniu8.com@163.com>
2018-11-17 11:16:10 +0800

    测试http请求

--- obd-web/src/test/java/com/wkhmedical/test/GGTest.java
+++ obd-web/src/test/java/com/wkhmedical/test/GGTest.java
@@ -3,8 +3,11 @@
  */
 package com.wkhmedical.test;
 
+import java.nio.charset.StandardCharsets;
+
 import org.junit.Test;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import com.taoxeo.repository.Paging;
@@ -18,16 +21,29 @@ public class GGTest {
 	/**
 	 * @param args
 	 */
-	@Test
-	public void main() {
+	public void test() {
 		RestTemplate rt = new RestTemplate();
 		String url = "http://test.5dwo.com/api/car/get.list?access_token=b81c9f5c-82be-4c77-a115-1779471b5047";
 		Paging<CarInfoPageParam> pgReq = new Paging<CarInfoPageParam>();
-		while (true) {
-			ResponseEntity<String> rseResult = rt.postForEntity(url, pgReq, String.class);
-			System.out.println(rseResult.getBody());
-		}
+		ResponseEntity<String> rseResult = rt.postForEntity(url, pgReq, String.class);
+		System.out.println(rseResult.getBody());
+	}
 
+	@Test
+	public void test1() {
+		RestTemplate template = new RestTemplate();
+		String url = "http://www.xzw.com/fortune/aries/1.html";
+
+//		HttpHeaders requestHeaders = new HttpHeaders();
+//		requestHeaders.add("User-Agent",
+//				"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.8 Safari/537.36");
+//		HttpEntity<String> requestEntity = new HttpEntity<String>(null, requestHeaders);
+//		ResponseEntity<String> response = template.exchange(url, HttpMethod.GET, requestEntity, String.class);
+		
+		template.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+		ResponseEntity<String> response = template.getForEntity(url, String.class);
+		String sttr = response.getBody();
+		System.out.println(sttr);
 	}
 
 }
